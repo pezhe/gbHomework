88918b4583494a730625bbf597341f7c25624147
pezhe <pezhe@list.ru>
2021-05-28 00:18:20 +0300

    Completed exercises 1-3 of Lesson4

--- src/main/java/ru/geekbrains/lesson4/TicTacToe.java
+++ src/main/java/ru/geekbrains/lesson4/TicTacToe.java
@@ -1,44 +1,47 @@
 package ru.geekbrains.lesson4;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
 
     static final char EMPTY_CELL_SYMBOL = '-';
+    static final int fieldSize = 3;
 
     public static void start() {
-        char[][] field = {
-                {EMPTY_CELL_SYMBOL, EMPTY_CELL_SYMBOL, EMPTY_CELL_SYMBOL},
-                {EMPTY_CELL_SYMBOL, EMPTY_CELL_SYMBOL, EMPTY_CELL_SYMBOL},
-                {EMPTY_CELL_SYMBOL, EMPTY_CELL_SYMBOL, EMPTY_CELL_SYMBOL}
-        };
+
+        char[][] field = createField();
 
         drawField(field);
 
-        do {
-            doPlayerMove(field);
-            drawField(field);
-            if (isWin(field, 'X')) {
-                System.out.println("Congratulations!!! You are winner.");
-                break;
-            }
-            if (isDraw(field)) {
-                System.out.println("This is draw!!!");
-                break;
-            }
+        try {
+            do {
+                doPlayerMove(field);
+                drawField(field);
+                if (isWin(field, 'X')) {
+                    System.out.println("Congratulations!!! You are winner.");
+                    break;
+                }
+                if (isDraw(field)) {
+                    System.out.println("This is draw!!!");
+                    break;
+                }
 
-            doBotMove(field);
-            drawField(field);
-            if (isWin(field, 'O')) {
-                System.out.println("Sorry!!! You are loser. :(");
-                break;
-            }
-            if (isDraw(field)) {
-                System.out.println("This is draw!!!");
-                break;
-            }
-        } while (true);
+                doBotMove(field);
+                drawField(field);
+                if (isWin(field, 'O')) {
+                    System.out.println("Sorry!!! You are loser. :(");
+                    break;
+                }
+                if (isDraw(field)) {
+                    System.out.println("This is draw!!!");
+                    break;
+                }
+            } while (true);
+        } catch (InputMismatchException e) {
+            System.out.println("exit");
+        }
 
     }
 
@@ -53,47 +56,39 @@ public class TicTacToe {
         return true;
     }
 
-    // boolean is = true
-    // !is -> false
-    // boolean isNot = false
-    // !isNot -> true
-
     static boolean isEmptyCell(char[][] field, int v, int h) {
         return field[v][h] == EMPTY_CELL_SYMBOL;
     }
 
     static boolean isNotEmptyCell(char[][] field, int v, int h) {
-        // if isEmptyCell -> false -> conversion !false ->  true
         return !isEmptyCell(field, v, h);
     }
 
     static boolean isWin(char[][] field, char sign) {
-        // Horizontal
-        for (int i = 0; i < field.length; i++) {
-            if (field[i][0] == sign && field[i][1] == sign && field[i][2] == sign) {
-                return true;
-            }
-            //   0 1 2
-            // 0 - - -     <- i = 0
-            // 1 - - -     <- i = 1
-            // 2 - - -     <- i = 2
-        }
-
-        // Vertical
-        for (int i = 0; i < field.length; i++) {
-            if (field[0][i] == sign && field[1][i] == sign && field[2][i] == sign) {
-                return true;
+        //Horizontal
+        for (char[] chars : field) {
+            for (int j = 0; j < field.length; j++) {
+                if (chars[j] != sign) break;
+                if (j == field.length - 1) return true;
             }
         }
-
-        // Diagonals
-        if (field[0][0] == sign && field[1][1] == sign && field[2][2] == sign) {
-            return true;
+        //Vertical
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length; j++) {
+                if (field[j][i] != sign) break;
+                if (j == field.length - 1) return true;
+            }
         }
-        if (field[0][2] == sign && field[1][1] == sign && field[2][0] == sign) {
-            return true;
+        //Main Diagonal
+        for (int i = 0; i < field.length; i++) {
+            if (field[i][i] != sign) break;
+            if (i == field.length - 1) return true;
         }
-
+        //Side Diagonal
+        for (int i = 0; i < field.length; i++) {
+            if (field[i][field.length - i - 1] != sign) break;
+            if (i == field.length - 1) return true;
+        }
         return false;
     }
 
@@ -101,9 +96,10 @@ public class TicTacToe {
         Random random = new Random();
 
         int v, h;
+
         do {
-            v = random.nextInt(3);
-            h = random.nextInt(3);
+            v = random.nextInt(fieldSize);
+            h = random.nextInt(fieldSize);
         } while (isNotEmptyCell(field, v, h));
 
         field[v][h] = 'O';
@@ -124,11 +120,24 @@ public class TicTacToe {
 
         int coordinate;
         do {
-            System.out.printf("Please enter %s-coordinate [1..3] ...%n", coordinateName);
+            System.out.printf("Please enter %s-coordinate [1..%d] ...%n", coordinateName, fieldSize);
             coordinate = scanner.nextInt() - 1;
         } while (coordinate < 0 || coordinate >= field.length);
 
         return coordinate;
+    }
+
+    static char[][] createField() {
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.print("Please enter field size...");
+        //fieldSize = scanner.nextInt();
+        char[][] field = new char[fieldSize][fieldSize];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length; j++) {
+                field[i][j] = EMPTY_CELL_SYMBOL;
+            }
+        }
+        return field;
     }
 
     static void drawField(char[][] field) {
