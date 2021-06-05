122ca933b3c63e5fdc3c48acc5cf9c5d4c69eab4
pezhe <pezhe@list.ru>
2021-06-05 12:58:28 +0300

    Lesson 7 homework base commit

---
+++ src/main/java/ru/geekbrains/lesson7/Plate.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson7;
+
+public class Plate {
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+}
