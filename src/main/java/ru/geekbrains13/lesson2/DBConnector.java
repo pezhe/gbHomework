421b90cb20e77b9c59c46e6b7fba8f3d839dd56e
pezhe <pezhe@list.ru>
2021-10-03 18:12:39 +0300

    Lesson 2 homework done (fix)

--- src/main/java/ru/geekbrains13/lesson2/DBConnector.java
+++ src/main/java/ru/geekbrains13/lesson2/DBConnector.java
@@ -4,24 +4,6 @@ import java.sql.*;
 
 public class DBConnector {
 
-    public static void main(String[] args) {
-        Connection connection = DBConnector.getConnection();
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM entry");
-            while (rs.next()) {
-                System.out.print(rs.getString(1) + " ");
-                System.out.print(rs.getString(2) + " ");
-                System.out.print(rs.getString(3));
-                System.out.println();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        } finally {
-            DBConnector.close(connection);
-        }
-    }
-
     public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection(
